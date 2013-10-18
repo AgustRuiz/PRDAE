a053183fca374de7cf1d4e8a07a865bcd67bb601
David Galisteo <darkvidvsgalis@gmail.com>
2013-10-19 00:22:41 +0200

    Hotel.java TERMINADO

--- src/java/es/ujaen/dae/galisteo_ruiz/hoteles/Hotel.java
+++ src/java/es/ujaen/dae/galisteo_ruiz/hoteles/Hotel.java
@@ -1,14 +1,8 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package es.ujaen.dae.galisteo_ruiz.hoteles;
 
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
-import java.util.Map;
-import java.util.TreeMap;
 
 /**
  *
@@ -23,71 +17,135 @@ public class Hotel {
     private String CIF;
     //cada posición del vector representa el número de individuales, dobles y triples
     private int[] numHabitaciones = new int[3];
-
-    public int[] getNumHabitaciones() {
-        return numHabitaciones;
-    }
-
-    public void setNumHabitaciones(int[] numHabitaciones) {
-        this.numHabitaciones = numHabitaciones;
-    }
-
-    public float[] getPrecioHabitaciones() {
-        return precioHabitaciones;
-    }
-
-    public void setPrecioHabitaciones(float[] precioHabitaciones) {
-        this.precioHabitaciones = precioHabitaciones;
-    }
     //cada posición del vector representa el precio de hab individual, doble y triple
     private float[] precioHabitaciones = new float[3];
     private List<Reserva> reservas;
 
+    /**
+     * Obtiene el vector de habitaciones
+     *
+     * @return vector de habitaciones
+     */
+    public int[] getNumHabitaciones() {
+        return numHabitaciones;
+    }
+
+    /**
+     * Establece el vector de habitaciones
+     *
+     * @param numHabitaciones vector numero de habitaciones
+     */
+    public void setNumHabitaciones(int[] numHabitaciones) {
+        this.numHabitaciones = numHabitaciones;
+    }
+
+    /**
+     * Obtiene el vector de precios
+     *
+     * @return vector de precios
+     */
+    public float[] getPrecioHabitaciones() {
+        return precioHabitaciones;
+    }
+
+    /**
+     * Establece el vector de precios
+     *
+     * @param precioHabitaciones vector precios de habitaciones
+     */
+    public void setPrecioHabitaciones(float[] precioHabitaciones) {
+        this.precioHabitaciones = precioHabitaciones;
+    }
+    
+    /**
+     * Obtiene la lista de reservas
+     *
+     * @return lista de reservas
+     */
     public List<Reserva> getReservas() {
         return reservas;
     }
     
-    public boolean reserva(Reserva res){
-        reservas.add(res);
-        return true;
-    }
-    
-    public int totalHabitaciones(){
-        return numHabitaciones[0]+numHabitaciones[1]+numHabitaciones[2];
-    }
-    
+    /**
+     * Obtiene el CIF
+     *
+     * @return CIF
+     */
     public String getCIF() {
         return CIF;
     }
 
+    /**
+     * Establece el CIF
+     *
+     * @param CIF 
+     */
+    public void setCIF(String CIF) {
+        this.CIF = CIF;
+    }
+    
+    /**
+     * Obtiene el nombre
+     *
+     * @return nombre
+     */
     public String getNombre() {
         return nombre;
     }
 
+    /**
+     * Establece el nombre
+     *
+     * @param nombre
+     */
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
 
+    /**
+     * Obtiene la direccion
+     *
+     * @return direccion
+     */
     public String getDireccion() {
         return direccion;
     }
 
-    public void setCIF(String CIF) {
-        this.CIF = CIF;
-    }
-
+    /**
+     * Establece la direccion
+     *
+     * @param direccion
+     */
     public void setDireccion(String direccion) {
         this.direccion = direccion;
     }
 
+    /**
+     * Obtiene la ciudad
+     *
+     * @return ciudad
+     */
     public String getCiudad() {
         return ciudad;
     }
 
+    /**
+     * Establece la ciudad
+     *
+     * @param ciudad
+     */
     public void setCiudad(String ciudad) {
         this.ciudad = ciudad;
     }
-
+    
+    /**
+     * comprueba si hay plazas para un tipo de habitación en un intervalo de fechas
+     * @param numHab
+     * @param tipoHab
+     * @param fIni
+     * @param fFin
+     * @return hay
+     */
     public boolean hayPlazas(int numHab, int tipoHab, Date fIni, Date fFin){
         int counter=0;
         boolean hay=true;
@@ -111,19 +169,39 @@ public class Hotel {
         return hay;
     }
     
+    /**
+     * Añade una nueva reserva a la lista
+     * @param res
+     */
+    public void reserva(Reserva res){
+        reservas.add(res);
+    }
+    
+    /**
+     * Constructor por defecto
+     */
     public Hotel() {
         numHabitaciones[0]=11; numHabitaciones[1]=1; numHabitaciones[2]=1;
         precioHabitaciones[0]=20; precioHabitaciones[1]=30; precioHabitaciones[2]=40;
         reservas = new ArrayList<Reserva>();
     }
 
-    public Hotel(String cif, String nom, String dir, String ciu, int[] n_hab, float[] p_hab) {
+    /**
+     * Constructor sobrecargado
+     * @param cif
+     * @param nom
+     * @param dir
+     * @param ciu
+     * @param nHab
+     * @param pHab 
+     */
+    public Hotel(String cif, String nom, String dir, String ciu, int[] nHab, float[] pHab) {
         CIF = cif;
         nombre = nom;
         direccion = dir;
         ciudad = ciu;
-        numHabitaciones = n_hab;
-        precioHabitaciones = p_hab;
+        numHabitaciones = nHab;
+        precioHabitaciones = pHab;
         reservas = new ArrayList<Reserva>();
     }
 }
